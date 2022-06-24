6c55a2a4e4162e5b2f2bac762338bfc792da7557
mj <855434@gmail.com>
2022-06-24 14:26:25 +0300

    add S folder

---
+++ CSharp/S/1/Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Input 1st integer");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input 2nd integer");
+int second = Convert.ToInt32(Console.ReadLine());
+
