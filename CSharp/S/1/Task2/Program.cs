1bc2c5da24912c20369b23687ca11c800e5e30b8
mj <855434@gmail.com>
2022-06-26 10:57:41 +0300

    change WriteLine to Write

--- CSharp/S/1/Task2/Program.cs
+++ CSharp/S/1/Task2/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите первое число");
+Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число");
+Console.Write("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 
 if ((a > b) && (a > c))
