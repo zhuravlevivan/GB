72dc55969d6d34e04d3386bd829fada49f3fa9e5
mj <855434@gmail.com>
2022-06-26 10:44:19 +0300

    Задача 4 Семинар 1

---
+++ CSharp/S/1/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((a > b) && (a > c))
+Console.WriteLine($"Максимальное введенное число: {a}");
+if ((a > b) && (a < c))
+Console.WriteLine($"Максимальное введенное число: {c}");
+if ((a < b) && (b < c))
+Console.WriteLine($"Максимальное введенное число: {c}");
+if ((a < b) && (b > c))
+Console.WriteLine($"Максимальное введенное число: {b}");
