ac0ee784ac5f0f560242ebb1fbc23755e0703e5c
mj <855434@gmail.com>
2022-06-26 14:30:34 +0300

    Добавил альтернативный вариант нахождения максимума

--- CSharp/S/1/Task2/Program.cs
+++ CSharp/S/1/Task2/Program.cs
@@ -7,11 +7,18 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 
-if ((a > b) && (a > c))
-Console.WriteLine($"Максимальное введенное число: {a}");
-if ((a > b) && (a < c))
-Console.WriteLine($"Максимальное введенное число: {c}");
-if ((a < b) && (b < c))
-Console.WriteLine($"Максимальное введенное число: {c}");
-if ((a < b) && (b > c))
-Console.WriteLine($"Максимальное введенное число: {b}");
+// if ((a > b) && (a > c))
+// Console.WriteLine($"Максимальное введенное число: {a}");
+// if ((a > b) && (a < c))
+// Console.WriteLine($"Максимальное введенное число: {c}");
+// if ((a < b) && (b < c))
+// Console.WriteLine($"Максимальное введенное число: {c}");
+// if ((a < b) && (b > c))
+// Console.WriteLine($"Максимальное введенное число: {b}");
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"Максимальное введенное число: {max}");
