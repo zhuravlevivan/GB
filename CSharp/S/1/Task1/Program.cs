281023336de4e94d23dd7064b8043d6302eb509d
mj <855434@gmail.com>
2022-06-26 10:57:31 +0300

    change WriteLine to Write

--- CSharp/S/1/Task1/Program.cs
+++ CSharp/S/1/Task1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 
-Console.WriteLine("Введите первое число");
+Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 if (a > b)
