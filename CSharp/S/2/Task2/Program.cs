afa050784d0a00cd1198cd241d569a7acc1670d3
mj <855434@gmail.com>
2022-06-27 23:04:09 +0300

    add task 2

---
+++ CSharp/S/2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GeekBrains
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintArray = Console.WriteLine(Console.ReadLine().ToString().ToCharArray());
+        }
+        int [] array = New [10];
+        //  public static int GetRandomNumber(int 100, int 1000)
+        // {
+        //     int number = new Random().Next(1, 999);
+        //     return number;
+        // }
+        // PrintArray[0, 2]
+    }
+}
+
