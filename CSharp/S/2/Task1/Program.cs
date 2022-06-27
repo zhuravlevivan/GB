06334e757a0bcb961ec911d3f20d4b6d49d5bcf9
mj <855434@gmail.com>
2022-06-27 23:03:58 +0300

    add task 1

---
+++ CSharp/S/2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GeekBrains
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+            // Вызов метода и получение результатов от метода
+            int number = GetRandomNumber(10, 100);
+            Console.WriteLine("Number: " + number);
+            // Приведение number к строке
+            string str = number.ToString(str[0].ToString());
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (result < Convert.ToInt32(str[i].ToString()))
+                {
+                    result = Convert.ToInt32(str[i].ToString());
+                }
+            }
+            Console.WriteLine("Result: " + result);
+        }
+
+        public static int GetRandomNumber(int first, int second)
+        {
+            int number = new Random().Next(first, second);
+            return number;
+        }
+    }
+}
