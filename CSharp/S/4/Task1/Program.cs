47880a91f30805381b5b5a870e0613c472f81f2b
mj <855434@gmail.com>
2022-07-04 22:57:51 +0300

    Add Seminar tasks

---
+++ CSharp/S/4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120 
+
+namespace Seminar_4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+        Console.WriteLine("Input Digit");
+        
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = s.Split(' ');    
+            
+        }
+
+    }
+}
