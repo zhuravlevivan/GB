852ec359293cc7082c6e78ba56f764e3c205c560
mj <855434@gmail.com>
2022-07-05 23:20:33 +0300

    add comments

--- CSharp/S/4/DZ_29/Program.cs
+++ CSharp/S/4/DZ_29/Program.cs
@@ -7,40 +7,39 @@ namespace DZ_29
 {
     class Program
     {
-
+        // Создаем функцию заполнения массива случайными числами (от 1 до 20).
+        // И выводим его в консоль для наглядности
         public static void FillArray(int[] array)
         {
-            Console.Write("Массив ");
+            Console.Write("Массив:   ");
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = new Random().Next(1, 20);
+                array[i] = new Random().Next(1, 21);
                 Console.Write($"{array[i]} ");
             }
-            
         }
+        // Создаем функцию возведения в квадрат значений массива поиндексно.
+        // Выводим полученные значения в консоль 
+        public static void GetSquares(int[] array_n)
+        {                
+            Console.WriteLine(); // Перенос строки
+            Console.Write("Квадраты: ");
+            for (int i = 0; i < array_n.Length; i++)
+            {
+                array_n[i] = Convert.ToInt32(Math.Pow(array_n[i], 2));
+                Console.Write($"{array_n[i]} ");
+            }
+        }
+        // Создаем массив случайного размера (от 1 до 15)
+        // Вызываем по очереди созданные ранее функции. В параметрах указываем созданный массив
         public static void Main()
         {
-
-            int a = new Random().Next(1, 50);
-            int[] numbers = new int[a];
-            int[] squares = new int[a];
-            
+            int[] numbers = new int[new Random().Next(1, 15)];
+                        
             FillArray(numbers);
-            
-            
-            
+            GetSquares(numbers);
             Console.WriteLine();
-            Console.Write("Квадраты ");
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = Convert.ToInt32(Math.Pow(numbers[i], 2));
-                squares[i] = numbers[i];
-
-                Console.Write($"{squares[i]} ");
-
-            }
-
         }
-    
+        
     }
 }
