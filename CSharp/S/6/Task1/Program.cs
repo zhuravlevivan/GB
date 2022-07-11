f4b2f07fb000490e51811fd2b618c887b84a0d16
zhuravlevivan <855434@gmail.com>
2022-07-11 22:42:13 +0300

    Seminar 5 Task1

---
+++ CSharp/S/6/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// 1. Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1] [6 7 3 6] -> [6 3 7 6] 
+
+namespace S6Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            
+            int[] arr = new int[new Random().Next(1,21)];
+            Fill(arr);
+            int[] rra = new int[arr.Length];
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                rra[i] = arr[^(i+1)];
+            }
+            Print(arr);
+            Console.WriteLine();
+            Print(rra);
+            Console.WriteLine();
+        }
+
+        public static void Fill(int[] nums)   
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = (new Random().Next(1, 100));
+            }
+        }
+
+        public static void Print(int[] nums)
+        {
+        	for(int i = 0; i < nums.Length; i++)
+        	{
+        		Console.Write($"{nums[i]} ");
+        	}
+        }
+    }
+}
