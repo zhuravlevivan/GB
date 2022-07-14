fac77ec5798090253c1c43883868bb96bc27217a
zhuravlevivan <855434@gmail.com>
2022-07-14 23:16:29 +0300

    Seminar 7

---
+++ CSharp/S/7/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// List<int> list = new List<int>() { 1, 2, 4, 5 };
+//             list.Add(6);
+//             list.Add(7);
+//             list.Add(10);
+
+//             foreach (int number in list)
+//             {
+//                 Console.WriteLine(number);
+//             }
+
+//             int num = list.Find(x => x % 2 == 0);
+
+
+// 1.Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₘₙ = m + n.
+
+// Выведите полученный массив на экран. m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5 
+
+
+
+namespace S7Task1
+{
+    class Program
+    {
+        ///<summary>Метод Main</summary>
+        public static void Main()
+        {
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
