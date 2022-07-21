9a2b84e8cd8131096b17c67b9ae9c387b29c568b
zhuravlevivan <855434@gmail.com>
2022-07-21 15:59:30 +0300

    Task1 Seminar 7

--- CSharp/S/7/Task1/Program.cs
+++ CSharp/S/7/Task1/Program.cs
@@ -1,17 +1,4 @@
-﻿// List<int> list = new List<int>() { 1, 2, 4, 5 };
-//             list.Add(6);
-//             list.Add(7);
-//             list.Add(10);
-
-//             foreach (int number in list)
-//             {
-//                 Console.WriteLine(number);
-//             }
-
-//             int num = list.Find(x => x % 2 == 0);
-
-
-// 1.Задайте двумерный массив размера m на n,
+﻿// 1.Задайте двумерный массив размера m на n,
 // каждый элемент в массиве находится по формуле: Aₘₙ = m + n.
 
 // Выведите полученный массив на экран. m = 3, n = 4.
@@ -19,20 +6,44 @@
 // 1 2 3 4
 // 2 3 4 5 
 
-
-
-namespace S7Task1
+namespace S7T1
 {
     class Program
     {
-        ///<summary>Метод Main</summary>
         public static void Main()
         {
+            Console.Write("Введите размерность двумерного массива через пробел: ");
+            string[] arr = Console.ReadLine().Split();
 
+            int a = int.Parse(arr[0]);
+            int b = int.Parse(arr[1]);
+            int[,] numbers = new int[a, b];
 
-
+            ArrayAmn(numbers);
+            PrintArray(numbers);
         }
 
+        public static void PrintArray(int[,] someArray)
+        {
+            for (int i = 0; i < someArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < someArray.GetLength(1); j++)
+                {
+                    Console.Write("{0,4}", someArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
 
+        public static void ArrayAmn(int[,] someArray)
+        {
+            for (int i = 0; i < someArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < someArray.GetLength(1); j++)
+                {
+                    someArray[i, j] = i + j;
+                }
+            }
+        }
     }
 }
