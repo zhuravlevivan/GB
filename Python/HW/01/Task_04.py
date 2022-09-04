2fda1a29ce3dd8a07c0105198f003771dcb83789
zhuravlevivan <855434@gmail.com>
2022-09-04 14:01:42 +0300

    Task_04 formatted

--- Python/HW/01/Task_04.py
+++ Python/HW/01/Task_04.py
@@ -9,9 +9,11 @@
 
 from math import sqrt as sqroot
 
-ax = int(input('Input x dot a'))
-ay = int(input('Input x dot b'))
-bx = int(input('Input y dot a'))
-by = int(input('Input y dot b'))
+ax = int(input('Input x dot a: '))
+ay = int(input('Input y dot a: '))
+bx = int(input('Input x dot b: '))
+by = int(input('Input y dot b: '))
 
 ab = sqroot(((bx - ax) ** 2) + ((by - ay) ** 2))
+
+print(round(ab, 2))
