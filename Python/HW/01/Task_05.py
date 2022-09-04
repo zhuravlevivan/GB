c8a0fc8c1190a491fed2bd25f8de76d15237c6c3
zhuravlevivan <855434@gmail.com>
2022-09-04 13:57:19 +0300

    add Task Predicate

--- Python/HW/01/Task_05.py
+++ Python/HW/01/Task_05.py
@@ -2,10 +2,11 @@
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
 # для всех значений предикат.
 
-x = int(input('Input x'))
-y = int(input('Input y'))
-z = int(input('Input z'))
-
-pr1 = not (x + y + z)
-pr2 = (not x) * (not y) * (not z)
-pr = pr1 == pr2
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            left = not (x or y or z)
+            right = (not x) and (not y) and (not z)
+            final = left == right
+            print(
+                f'При x = {x}, y = {y}, z = {z} [ {left} = {right} ] \t {final}')
