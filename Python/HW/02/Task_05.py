d2c163b876a29e6fb4703e5e3a71406334bce54d
zhuravlevivan <855434@gmail.com>
2022-09-07 23:00:24 +0300

    dop

---
+++ Python/HW/02/Task_05.py
@@ -0,0 +1,12 @@
+kust = []
+
+n = int(input('Введите количество кустов: '))
+for i in range(1, n+1):
+    kust.append(i)
+
+kust.sort(reverse=True)
+
+summ = 0
+for i in range(n-1):
+    summ = summ + kust[i]
+print(summ)
