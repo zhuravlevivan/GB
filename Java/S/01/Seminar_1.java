be69598ebfea1d1bba0a4e17d7a6691c5794cdd1
Mj <855434@gmail.com>
2022-12-07 21:08:31 +0300

    added sorted & раза

--- Java/S/01/Seminar_1.java
+++ Java/S/01/Seminar_1.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Seminar_1 {
     public static void main(String[] args) {
         int[] a = { 1, 9, 9, 0, 2, 8, 0, 9 };
-        int[] b = uniqueArray(a);
+        int[] b = uniqueSortedArray(a);
 
         for (int i = 0; i < b.length; i++) {
             int count = 0;
@@ -12,10 +12,13 @@ public class Seminar_1 {
                     count++;
                 }
             }
-            System.out.printf("%d count %d\n", b[i], count);
+            if (count > 1 && count < 5) {
+                System.out.printf("%d встречается %d раза\n", b[i], count);
+            }
+            else {System.out.printf("%d встречается %d раз\n", b[i], count);}
         }
     }
-    public static int[] uniqueArray (int [] a) {
-        return Arrays.stream(a).distinct().toArray();
+    public static int[] uniqueSortedArray (int [] a) {
+        return Arrays.stream(a).distinct().sorted().toArray();
     }
 }
