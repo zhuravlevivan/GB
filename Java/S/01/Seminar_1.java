5c980fc5c36a1744e88babafc9acb39fca240c8c
Mj <855434@gmail.com>
2022-12-07 21:26:26 +0300

    reformat

--- Java/S/01/Seminar_1.java
+++ Java/S/01/Seminar_1.java
@@ -2,27 +2,29 @@ import java.util.Arrays;
 
 public class Seminar_1 {
     public static void main(String[] args) {
-        int[] a = { 1, 9, 9, 0, 2, 8, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+        int[] a = {1, 9, 9, 0, 2, 8, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
         result(a);
     }
-    public static int[] uniqueSortedArray (int [] a) {
+
+    public static int[] uniqueSortedArray(int[] a) {
         return Arrays.stream(a).distinct().sorted().toArray();
     }
 
-    public static void result (int[] a) {
+    public static void result(int[] a) {
         int[] b = uniqueSortedArray(a);
 
-        for (int i = 0; i < b.length; i++) {
+        for (int i : b) {
             int count = 0;
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] == b[i]) {
+            for (int j : a) {
+                if (j == i) {
                     count++;
                 }
             }
             if (count > 1 && count < 5 || count % 10 > 1 && count % 10 < 5) {
-                System.out.printf("%d встречается %d раза\n", b[i], count);
+                System.out.printf("%d встречается %d раза\n", i, count);
+            } else {
+                System.out.printf("%d встречается %d раз\n", i, count);
             }
-            else {System.out.printf("%d встречается %d раз\n", b[i], count);}
         }
     }
 }
