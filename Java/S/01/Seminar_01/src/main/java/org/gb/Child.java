74a82d774e4b310b19a9ea1f958375a5d351d834
legion <855434@gmail.com>
2023-01-16 21:50:19 +0300

    Seminar_01

---
+++ Java/S/01/Seminar_01/src/main/java/org/gb/Child.java
@@ -0,0 +1,62 @@
+package org.gb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Child {
+    private String fi;
+    private String dateOfBirth;
+    private String city;
+    private String country;
+
+    public String getFi() {
+        return fi;
+    }
+
+    public void setFi(String fi) {
+        this.fi = fi;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    Child(String fi, String dateOfBirth, String city, String country) {
+        this.fi = fi;
+        this.dateOfBirth = dateOfBirth;
+        this.city = city;
+        this.country = country;
+    }
+
+    Child(){
+
+    }
+
+   public void childInfo(){
+       System.out.printf(" Name %s%n date of birth %s%n city %s%n country %s%n",
+               fi,
+               dateOfBirth,
+               city,
+               country);
+   }
+}
---
+++ Java/S/01/Seminar_01/src/main/java/org/gb/Father.java
@@ -0,0 +1,39 @@
+package org.gb;
+
+public class Father extends Child{
+
+    private String childFi;
+
+    Father(String fi, String dateOfBirth, String city, String country, String childFi) {
+        super(fi, dateOfBirth, city, country);
+        setChildFi(childFi);
+    }
+
+    Father(String childFi){
+        super();
+        setChildFi(childFi);
+    }
+
+    public String getChildFi() {
+        return childFi;
+    }
+
+    public void setChildFi(String childFi) {
+        this.childFi = childFi;
+    }
+
+    public void fatherInfo(){
+        childInfo();
+        getChildFi();
+    }
+
+    @Override
+    public void childInfo() {
+        System.out.printf("Name %s%ndate of birth %s%ncity %s%ncountry %s%nChildName %s%n",
+                super.getFi(),
+                super.getDateOfBirth(),
+                super.getCity(),
+                super.getCountry(),
+                getChildFi());
+    }
+}
