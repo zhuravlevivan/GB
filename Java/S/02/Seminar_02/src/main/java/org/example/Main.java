59bef8322d5b8c81a8fd75dc2db7e7ccb496327e
legion <855434@gmail.com>
2023-01-19 21:11:26 +0300

    add Main

---
+++ Java/S/02/Seminar_02/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Father father = new Father("FatherName SecondFatherName",
+                "15.01.1987",
+                "MSK",
+                "Russia",
+                "ChildName");
+
+        Child child = new Child("ChildName ChildSecondName",
+                "12.02.2011",
+                "MSK",
+                "Russia");
+
+        father.fatherInfo();
+        child.getInfo();
+
+    }
+}
